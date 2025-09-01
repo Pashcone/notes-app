a84b9d0f2e79f970384c1ecee7e205a5d98c135c
Pashcone <pascatudor@gmail.com>
2025-09-01 13:35:00 +0300

    uibuild

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,25 +1,10 @@
-import mongoose from 'mongoose';
-import dotenv from "dotenv";
-mongoose.set('strictQuery', false)
-
-dotenv.config();
-const url = process.env.MONGODB_URI;
-
-
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -32,4 +17,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-export default mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
